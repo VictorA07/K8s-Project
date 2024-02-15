51ccb681998a2d4e478bced5cc103d7b09f328de
VictorA07 <victor.adepoju@cloudhight.com>
2024-02-15 21:03:11 +0000

    infra code

---
+++ jenkins/main.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_subnet" "name" {
+  
+}
+
+resource "aws_internet_gateway" "name" {
+  
+}
+
+resource "aws_route_table" "name" {
+  
+}
+
+resource "aws_route_table_association" "name" {
+  
+}
+
+resource "aws_nat_gateway" "name" {
+  
+}
+
+resource "aws_eip" "name" {
+  
+}
+
+resource "aws_security_group" "name" {
+  
+}
+
+resource "tls_private_key" "name" {
+  
+}
+
+resource "local_file" "name" {
+  
+}
+resource "aws_key_pair" "name" {
+  
+}
+
+resource "" "name" {
+  
+}
